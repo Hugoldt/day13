9a6b41654aeeb971e9a46b639a447063add5268c
Hugoldt <hugo.ladrat@epitech.digital>
2024-10-18 21:15:14 +0200

    Add files via upload

---
+++ ex_01bis.js
@@ -0,0 +1,27 @@
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    let firstName = document.getElementById('firstname').value;
+    let lastName = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]+$/;
+    let passwordRegex = /^(?=.[a-z])(?=.[0-9]).{8,}$/;
+
+    if (firstName === "" || lastName === "") {
+        document.getElementById('error').innerHTML = 'First name and last name cannot be empty.';
+        return;
+    }
+
+    if (!emailRegex.test(email)) {
+        document.getElementById('error').innerHTML = 'Email is badly formatted.';
+        return;
+    }
+
+    if (!passwordRegex.test(password)) {
+        document.getElementById('error').innerHTML = 'Password must be at least 8 characters long and contain at least a letter and a number.';
+
+    }
+}
